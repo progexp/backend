24e95215e8a0b242096ec39e58a7141f3b1efde7
mortyhwk <142350770+mortyhwk@users.noreply.github.com>
2024-06-04 19:07:56 +0200

    add static imgs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     app.use(cookieParser());
     app.setGlobalPrefix('api');
@@ -13,6 +15,9 @@ async function bootstrap() {
         origin: ['http://localhost:3000'],
         credentials: true,
         exposedHeaders: 'set-cookie'
+    });
+    app.useStaticAssets(join(__dirname, '../../../', 'public'), {
+        prefix: '/static/'
     });
 
     await app.listen(4200);
